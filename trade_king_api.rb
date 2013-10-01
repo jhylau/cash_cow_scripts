b5152f5143f37198c7f6f3bd23a51def01ab2d82
Jonathan Lau <jhylau1@gmail.com>
2013-09-30 23:41:34 -0400

    added parser for stocks

--- trade_king_api.rb
+++ trade_king_api.rb
@@ -2,13 +2,14 @@
 require 'rubygems'
 require 'oauth'
 require 'pry'
+require 'JSON'
+require 'ox'
 
 # Your key/secrets for authentication
 CONSUMER_KEY        = ENV["CONSUMER_KEY"]
 CONSUMER_SECRET     = ENV["CONSUMER_SECRET"]
 ACCESS_TOKEN        = ENV["ACCESS_TOKEN"]
 ACCESS_TOKEN_SECRET = ENV["ACCESS_TOKEN_SECRET"]
-binding.pry
 
 # Set up an OAuth Consumer
 @consumer = OAuth::Consumer.new CONSUMER_KEY, CONSUMER_SECRET, { :site => 'https://api.tradeking.com' }
@@ -19,6 +20,14 @@ binding.pry
 
 # Make a request to the API endpoint and display the response
 
-puts @access_token.get('/v1/accounts.json', {'Accept' => 'application/json'}).body
-puts @access_token.get('/v1/accounts/60573697/balances.json', {'Accept' => 'application/json'}).body
-puts @access_token.get('/v1/accounts/60573697/balances.json', {'Accept' => 'application/json'}).body
+# puts @access_token.get('/v1/accounts.json', {'Accept' => 'application/json'}).body
+
+# gets my account info
+# puts @access_token.get('/v1/accounts/60573697/balances.json', {'Accept' => 'application/json'}).body
+
+# get stock info
+stocks = 'aapl,ibm'
+stock_info = @access_token.get("/v1/market/ext/quotes.xml?symbols=#{stocks}")
+
+# parse stock info
+data = Ox.parse(stock_info.body)
