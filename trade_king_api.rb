ce11064e9861dcb17587565d83f9ffdb813cad9c
Jonathan Lau <jhylau1@gmail.com>
2013-10-01 21:47:36 -0400

    added keys and sample parse line

--- trade_king_api.rb
+++ trade_king_api.rb
@@ -6,10 +6,10 @@ require 'JSON'
 require 'active_support/all'
 
 # Your key/secrets for authentication
-CONSUMER_KEY        = ENV["CONSUMER_KEY"]
-CONSUMER_SECRET     = ENV["CONSUMER_SECRET"]
-ACCESS_TOKEN        = ENV["ACCESS_TOKEN"]
-ACCESS_TOKEN_SECRET = ENV["ACCESS_TOKEN_SECRET"]
+CONSUMER_KEY='wxe7oBlTMLdkBEnW8R86ZaFonHORQ8GkHkNXf9dQ'
+CONSUMER_SECRET='0GWWRp7WGV9iKZasGbwibpMHamMvGMxerDrMk237'
+ACCESS_TOKEN='7Zo6O2CEis7PTaphPyAVllHTdft4O0gvYFHpszyY'
+ACCESS_TOKEN_SECRET='jNQfRvVBhawaDGUcQBBU3W83ybgzmZoiuUzcIb5x'
 
 # Set up an OAuth Consumer
 @consumer = OAuth::Consumer.new CONSUMER_KEY, CONSUMER_SECRET, { :site => 'https://api.tradeking.com' }
@@ -31,3 +31,4 @@ stock_info = @access_token.get("/v1/market/ext/quotes.xml?symbols=#{stocks}")
 
 # parse stock info
 hash = Hash.from_xml(stock_info.body)
+aapl_adp_100 = hash['response']['quotes']['quote'][0]["adp_100"]
